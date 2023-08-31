b4423b46befa39ecb7ef0b3812a7df9478a78609
Nikolay K <sirbulgat@gmail.com>
2023-09-01 01:08:22 +0300

    add collision chest

--- Assets/Script/ChestBox.cs
+++ Assets/Script/ChestBox.cs
@@ -7,17 +7,30 @@ public class ChestBox : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    /*
-   void OnControllerColliderHit(ControllerColliderHit hit)
+    private void OnCollisionEnter3D(Collision2D collision)
     {
-        Debug.Log("hit"+ hit);
-    }*/
+        Destroy(this);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+
+        Debug.Log(bullet+" collision = " + collision.gameObject.name);
+        if (bullet != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+       // Debug.Log("stay collision = "  );
+    }
 }
